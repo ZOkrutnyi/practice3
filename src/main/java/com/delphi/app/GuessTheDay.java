d62901583058dc28fc88b181e179cc8b0757a00e
ZOkrutnyi <leto6601@gmail.com>
2021-05-27 15:18:55 +0300

    ver 1.

--- src/main/java/com/delphi/app/GuessTheDay.java
+++ src/main/java/com/delphi/app/GuessTheDay.java
@@ -4,23 +4,28 @@ import static com.delphi.app.WeekDays.getDay;
 
 public class GuessTheDay {
     public static void main(String[] args) {
-        if(args.length>0) {
-            GuessTheDay g = new GuessTheDay();
-            g.weekDays(args[0]);
-        }else
-        System.out.println("You better try to run another program without parameters!");
+        if (args.length > 0) {
+            new GuessTheDay().findDay(args[0]);
+        } else
+            System.out.println("You better try to run another program without parameters!");
     }
-    public void weekDays(String wd)
-    {
-        System.out.println(isWeekend(getDay(wd)));
+
+    /**
+     * findDay(String wd) converts input String to WeekDays type
+     * and prints it`s weekend or not
+     */
+    public void findDay(String wd) {
+        System.out.println(weekDay(getDay(wd)));
     }
-    public static String isWeekend(int day)
-    {
-        if(day>=1&&day<=5) {
+
+    /**
+     * weekDay(int day) take the day
+     * if day exists return if it`s weekend or not
+     */
+    private String weekDay(int day) {
+        if (day >= 1 && day <= 5) {
             return "Again work day";
-        }
-        else if(day>=6&&day<=7)
-        {
+        } else if (day >= 6 && day <= 7) {
             return "Finally weekend";
         }
         return "Is it even day???";
